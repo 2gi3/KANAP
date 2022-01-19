e65080004b63858eb3232ff3491acadd20b383f0
2gi3 <gippolito@hotmail.co.uk>
2022-01-19 14:48:04 +0000

    homepage products DOM

---
+++ front/js/script.js
@@ -0,0 +1,34 @@
+// load all products from http://localhost:3000/api/products  api
+
+fetch('http://localhost:3000/api/products')
+    .then(response => {
+        response.json().then(data => {
+            console.log(data);
+            let productHTML ='';
+
+            // tag the product div
+            productsDiv = document.getElementById('items');
+
+            // loop through the array to build the html to be added to our dom
+            data.forEach(product =>{
+                productHTML +=` 
+                <a href="./product.html?id=${product._id}">
+                <article>
+                  <img src="${product.imageUrl}" alt="${product.altTxt}">
+                  <h3 class="productName">${product.name}</h3>
+                  <p class="productDescription">${product.description}</p>
+                </article>
+              </a>
+                `;
+
+            });
+            // modify the content of the div with the html produced
+           productsDiv.innerHTML = productHTML;
+       
+        })
+    })
+
+
+
+
+
