3a259d11344572d15bed9eba43886d499b449a84
2gi3 <gippolito@hotmail.co.uk>
2022-01-29 09:03:00 +0000

    javascript cart unfinished

--- front/js/product.js
+++ front/js/product.js
@@ -7,54 +7,71 @@ const urlParams = new URLSearchParams(queryString);
 
 const id = urlParams.get('id')
 
-
-
 fetch(`http://localhost:3000/api/products/${id} `)
     .then(response => {
         response.json().then(product => {
             console.log(product);
-            // let productHTML ='';
 
             // tag the product div
             let imgDiv = document.getElementsByClassName('item__img')[0];
-
             imgDiv.innerHTML = `  <img src="${product.imageUrl}" alt="Photographie d'un canapé">  `;
 
             let productTitle = document.getElementById("title");
-
-            productTitle.innerHTML =product.name;
+            productTitle.innerHTML = product.name;
 
             let productPrice = document.getElementById("price");
-
             productPrice.innerHTML = product.price;
 
             let productDescription = document.getElementById("description");
-
             productDescription.innerHTML = product.description;
 
             let colorsDropdown = document.getElementById("colors");
-
             product.colors.forEach(element => {
-
-                colorsDropdown.innerHTML +=`<option value="${element}">${element}</option>`;
-                
+                colorsDropdown.innerHTML += `<option value="${element}">${element}</option>`;
             });
-
-            // loop through the array to build the html to be added to our dom
-            // data.forEach(product =>{
-            //     productHTML +=` 
-            //     <a href="./product.html?id=${product._id}">
-            //     <article>
-            //       <img src="${product.imageUrl}" alt="${product.altTxt}">
-            //       <h3 class="productName">${product.name}</h3>
-            //       <p class="productDescription">${product.description}</p>
-            //     </article>
-            //   </a>
-            //     `;
-
-            // });
-            // modify the content of the div with the html produced
-        //    productsDiv.innerHTML = productHTML;
-       
         })
-    })
+    });
+
+
+    document.getElementById("addToCart").addEventListener("click", ()  => { 
+
+        let color = document.getElementById("colors").value;
+        let cart = localStorage.getItem("cart");
+        // check if color has been selected
+        if (color -""){
+            alert("please select a color")
+        } else {
+            let products = [];
+            // If a cart doesn't exist push object to array then store array in local storage
+            if (cart == null){
+                products.push( { _id:id, quantity:1, color:color } )
+
+                localStorage.setItem("cart", JSON.stringify(products))
+            }else{
+                // if there is already an object in the array then find if it hase same id
+                cart = JSON.parse(localStorage.getItem("cart"));
+                let index = cart.findIndex( object => object._id == id  );
+                // if id is the same then check color
+                if (index != -1) {
+                    // if color is the same then add 1 to quantity
+                    if(cart[index].color == color){
+                        cart[index].quantity += 1; 
+                    }else{ 
+                        // if color is not the same then add new object to array
+                        
+                        // cart.push( { _id:id, quantity:1, color:color});
+                        // localStorage.setItem(cart, JSON.stringify(cart));
+                    } 
+                }
+
+
+            }          
+
+        }
+       
+        //  { _id:id, quantity:1, color:color };
+
+
+    });
+
+
