a6e107710b529e74fb3bdd5932a91f52c4306cf4
2gi3 <gippolito@hotmail.co.uk>
2022-01-29 18:43:06 +0000

    add to cart done

--- front/js/product.js
+++ front/js/product.js
@@ -35,42 +35,36 @@ fetch(`http://localhost:3000/api/products/${id} `)
 
     document.getElementById("addToCart").addEventListener("click", ()  => { 
 
-        let color = document.getElementById("colors").value;
         let cart = localStorage.getItem("cart");
+        color = document.getElementById("colors").value;       
+        let quantity = document.getElementById("quantity").value;
         // check if color has been selected
-        if (color =""){
-            alert("please select a color")
+        if (color ==""){
+            alert("please select a color");
         } else {
             let products = [];
             // If a cart doesn't exist push object to array then store array in local storage
             if (cart == null){
-                products.push( { _id:id, quantity:1, color:color } )
-
+                products.push( { _id:id, quantity:parseInt(quantity), color:color } )
                 localStorage.setItem("cart", JSON.stringify(products))
             }else{
                 // if there is already an object in the array then find if it hase same id
                 cart = JSON.parse(localStorage.getItem("cart"));
-                let index = cart.findIndex( object => object._id == id  );
+                let index = cart.findIndex( object => object._id == id && object.color == color );
                 // if id is the same then check color
                 if (index != -1) {
                     // if color is the same then add 1 to quantity
-                    if(cart[index].color == color){
-                        cart[index].quantity += 1; 
-                    }else{ 
-                        // if color is not the same then add new object to array
-                        
-                        cart.push( { _id:id, quantity:1, color:color});
-                        localStorage.setItem(cart, JSON.stringify(cart));
-                    } 
+                    
+                        cart[index].quantity += parseInt(quantity); 
+                    
+                }else{
+                    cart.push( { _id:id, quantity:parseInt(quantity), color:color})
+                    
                 }
-
-
+                localStorage.setItem("cart", JSON.stringify(cart));
             }          
 
         }
-       
-        //  { _id:id, quantity:1, color:color };
-
 
     });
 
